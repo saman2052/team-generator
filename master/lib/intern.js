5f2fd3c612a23e9fd2a24b4f7086b219e730dfde
dsorthep <detvisiths@yahoo.com>
2020-01-15 16:59:02 -0600

    edited intern.js

--- master/lib/intern.js
+++ master/lib/intern.js
@@ -4,10 +4,9 @@ class Intern extends Employee {
     constructor(id, name, email, school) {
         super(id, name, email);
         this.school = school;
-        this.role = "Intern";
     }
     getRole(){
-        return this.role;
+        return "Intern";
     }
 
 
